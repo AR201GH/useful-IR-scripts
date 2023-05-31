715f31b3de473c369b68fb320a32968e03ef3d70
AR201GH <135081007+AR201GH@users.noreply.github.com>
2023-05-31 09:59:45 -0400

    Update decode-base64.py

--- Python/decode-base64.py
+++ Python/decode-base64.py
@@ -2,13 +2,10 @@
 import base64
 import sys
 
-#function to decode base64 in utf-8
+#function to decode base64 in utf-8 from user input
 def decode_base64():
-    #get input from user
     user_input = input("Enter base64 to decode: ")
-    #decode base64
     decode = base64.b64decode(user_input).decode('utf-8')
-    #print decoded base64
     print(decode)
 
 #print decoded base64
