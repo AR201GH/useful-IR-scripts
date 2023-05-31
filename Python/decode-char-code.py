b7d62556e7b5be5fe3a59a2be6de9dc262c4d4e8
AR201GH <135081007+AR201GH@users.noreply.github.com>
2023-05-30 21:24:00 -0400

    Update decode-char-code.py
    Sometimes malware will use character encoding to attempt to hide from analysts and security tools. This script will decode decimal representations of Unicode character encoding. Decimal Unicode representations must be converted if not already.

--- Python/decode-char-code.py
+++ Python/decode-char-code.py
@@ -1,4 +1,4 @@
-#string expects just decimal numbers, comma seperated with aspqce after the comma 
+#string expects only decimal numbers, comma seperated with aspqce after the comma 
 string = [dec1, dec2, dec3, etc]
 x  = ''.join([char(x) for x in string])
 print(x)
